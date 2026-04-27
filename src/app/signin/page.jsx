cacbe39da543a23a8433d0018de8be61262731f4
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-04-27 12:34:41 +0600

    Building Signup & Signin Pages, Logout

---
+++ src/app/signin/page.jsx
@@ -0,0 +1,94 @@
+"use client";
+import React from "react";
+import { Check } from "@gravity-ui/icons";
+import {
+  Button,
+  Description,
+  FieldError,
+  Form,
+  Input,
+  Label,
+  TextField,
+} from "@heroui/react";
+import { authClient } from "@/lib/auth-client";
+
+const SignInPage = () => {
+  const formSubmit = async (e) => {
+    e.preventDefault();
+
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    const { data, error } = await authClient.signIn.email({
+      email,
+      password,
+      callbackURL: "/",
+    });
+    console.log({ data, error });
+  };
+  return (
+    <Form
+      className="flex w-96 flex-col gap-4 mx-auto p-20 border-2 rounded-2xl shadow-2xl mt-10"
+      onSubmit={formSubmit}
+    >
+      {/* name
+      <TextField isRequired name="name" type="text">
+        <Label>Name</Label>
+        <Input placeholder="Please enter your full name" />
+        <FieldError />
+      </TextField> */}
+      {/* email */}
+      <TextField
+        isRequired
+        name="email"
+        type="email"
+        validate={(value) => {
+          if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+            return "Please enter a valid email address";
+          }
+          return null;
+        }}
+      >
+        <Label>Email</Label>
+        <Input placeholder="john@example.com" />
+        <FieldError />
+      </TextField>
+      <TextField
+        isRequired
+        minLength={8}
+        name="password"
+        type="password"
+        validate={(value) => {
+          if (value.length < 8) {
+            return "Password must be at least 8 characters";
+          }
+          if (!/[A-Z]/.test(value)) {
+            return "Password must contain at least one uppercase letter";
+          }
+          if (!/[0-9]/.test(value)) {
+            return "Password must contain at least one number";
+          }
+          return null;
+        }}
+      >
+        <Label>Password</Label>
+        <Input placeholder="Enter your password" />
+        <Description>
+          Must be at least 8 characters with 1 uppercase and 1 number
+        </Description>
+        <FieldError />
+      </TextField>
+      <div className="flex gap-2">
+        <Button type="submit">
+          <Check />
+          Submit
+        </Button>
+        <Button type="reset" variant="secondary">
+          Reset
+        </Button>
+      </div>
+    </Form>
+  );
+};
+
+export default SignInPage;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,44 @@
+"use client";
+import { authClient } from "@/lib/auth-client";
+import { Button } from "@heroui/react";
 import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
+  const { data: session } = authClient.useSession();
+  console.log("session from:", session);
+
+  const user = session?.user;
+  const handleSignOut = () => {
+    // alert("signout btn clicked");
+    authClient.signOut();
+  };
+
   return (
     <div className="border p-5 bg-zinc-700 flex justify-center gap-5 text-white">
-      <Link href={"/"}>Home</Link>
-      <Link href={"/models"}>Models</Link>
-      <Link href={"/signup"}>Sign up</Link>
+      <Link className="font-bold text-lg" href={"/"}>
+        Home
+      </Link>
+      <Link className="font-bold text-lg" href={"/models"}>
+        Models
+      </Link>
+      <Link className="font-bold text-lg" href={"/signup"}>
+        Sign up
+      </Link>
+      <Link className="font-bold text-lg" href={"/signin"}>
+        Sign in
+      </Link>
+      <div className="font-bold text-sm">
+        <div>{user?.name}</div>
+        <div>{user?.email}</div>
+      </div>
+      <Button
+        onClick={handleSignOut}
+        className="font-bold text-lg"
+        variant="danger-soft"
+      >
+        SignOut
+      </Button>
     </div>
   );
 };
